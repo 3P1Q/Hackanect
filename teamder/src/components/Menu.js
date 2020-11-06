3f9e545fb2211e98f41534afb78150e434f473b4
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2020-11-06 20:59:41 +0530

    add photo, menu and tech stack components

---
+++ teamder/src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = (props) => {
+    return <div>
+        <div className="menu">
+            <ul>
+                <li>Profile</li>
+                <li>Connect</li>
+                <li>Edit</li>
+                <li>Logout</li>
+            </ul>
+        </div>
+    </div>
+}
+
+export default Menu;
---
+++ teamder/src/components/NameAndAvatar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container, Avatar} from '@material-ui/core';
+import './App.css';
+
+const NameAndAvatar = (props) => {
+    function getInitials(name)
+    {
+        var initials = name.match(/\b\w/g) || [];
+        initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+        return initials;
+    }
+    return <div className="name-and-avatar">
+        <div className="avatar-container">
+            {props.src === "" ? <Avatar className="avatar">{getInitials(props.name)}</Avatar> : <Avatar className="avatar" alt={props.name} src={props.src} />}
+        </div>
+        <div className="name-container">
+            <h1> {props.name} </h1>
+        </div> 
+    </div>
+}
+
+export default NameAndAvatar;
