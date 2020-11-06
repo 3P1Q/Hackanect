7709602c5162508c410db112e94f8697b9dea201
sanskar-p <sanskarpatro2012@gmail.com>
2020-11-06 17:59:46 +0300

    added card component

--- teamder/src/components/App.js
+++ teamder/src/components/App.js
@@ -1,9 +1,20 @@
+import React, {Component} from 'react';
 import './App.css';
+import ProjectCard from './ProjectCard/ProjectCard'
 
-function App() {
-  return (
-    <h1>Welcome to Teamder</h1>
-  );
+//temporary json file(s)      (should be removed later)
+import projectcard from '../exampleJSONs/projectcard.json'
+
+class App extends Component {
+  render(){
+    return (
+    <div>
+      <h1>Welcome to Teamder</h1>
+      <ProjectCard deets={projectcard} />
+    </div>
+    );
+  }
+  
 }
 
 export default App;
