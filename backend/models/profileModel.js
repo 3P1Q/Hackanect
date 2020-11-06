b5e9ebeb8f72466142e5188aa33a85b4c6f94fde
uglyprincess <the.boy.who.lived.3x4@gmail.com>
2020-11-06 22:35:45 +0530

    Added Profile Model

--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -1,1 +1,21 @@
 // Mongoose profileModel goes here
+const { model } = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    profilePic: String,
+    about: String,
+    gender: String,
+    project: {title: String, link: String},
+    techStack: [String],
+    social: {github: String, facebook: String, linkedIn: String, twitter: String},
+    discord: String
+
+});
+
+module.exports = mongoose.model('userModel', userSchema);
+
+
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../models/profileModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -6,7 +7,35 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req,res) => {
-    res.send("Howdy-ho, this is the register page.");
+    
+    const freshUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        profilePic: req.body.image,
+        about: req.body.about,
+        gender: req.body.gender,
+        project: {
+            title: req.body.projectTitle,
+            link: req.body.projectLink
+        },
+        techStack: req.body.stackArray,
+        social: {
+            github: req.body.github,
+            facebook: req.body.fb,
+            linkedIn: req.body.linkedIn,
+            twitter: req.body.twitter
+        },
+        discord: req.body.twitter
+    });
+
+    freshUser.save(function(err){
+        if(err) {
+            console.log(err);
+        }
+    });
+    
+    // res.send("Howdy-ho, this is the register page.");
 });
 
 module.exports = router;
