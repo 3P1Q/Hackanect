9191f6c022a6bb8910504632641df9299171c12c
Ranveer251 <53096435+Ranveer251@users.noreply.github.com>
2020-12-09 19:25:37 +0530

    improve find or create method in oauth

--- teamder/src/components/OAuth/Github.js
+++ teamder/src/components/OAuth/Github.js
@@ -3,7 +3,7 @@ import {Redirect} from 'react-router-dom';
 const Github = (props) => {
     const id = props.routerProps.match.params.username;
     localStorage.setItem("username", id);
-    return <Redirect to={`/profile/${id}`} />
+    return id!== 'undefined' ? <Redirect to={`/profile/${id}`} /> : "Loading";
 }
 
 export default Github;
